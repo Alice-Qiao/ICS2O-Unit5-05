72d8471e79197109916d0e809d30bfda52d0ca6a
AliceQiao <alice.qiao@ocsbstudent.ca>
2021-03-18 13:54:34 +0000

    first

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.getElementById('button').addEventListener('click', math)
 
 function math () {
   number = document.getElementById('numero').value
-  number = number*number
+  number = number * number
   alert(number)
 }
